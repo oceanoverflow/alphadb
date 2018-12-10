b4a312c5ea8adbfd061e6492119189ad3dd3e55d
yiyangyi <oceanoverflow@gmail.com>
2018-12-10 16:12:05 +0800

    add txn manager base class for other txn manager to derive.

--- transaction/txn_manager.cc
+++ transaction/txn_manager.cc
@@ -1,9 +1,31 @@
 #include "txn_manager.h"
 
-txn_manager::txn_manager(/* args */)
+txn_manager::txn_manager(index_manager& idx_mgr, table_manager& tbl_mgr): idx_mgr_{idx_mgr}, tbl_mgr_{tbl_mgr} {}
+
+txn_manager::~txn_manager() {}
+
+table_handle txn_manager::get_table(const std::string& name)
 {
+    auto it = tables_.find(name);
+    if (it == tables_.end()) {
+        table_handle handle = tbl_mgr_.open_table(name.c_str());
+        tables_[name] = handle; 
+        return handle;
+    }
+    else {
+        return it->second;
+    }
 }
 
-txn_manager::~txn_manager()
+index_handle txn_manager::get_index(const std::string& name, int index)
 {
+    auto it = indexes_.find(name);
+    if (it == indexes_.end()) {
+        index_handle handle = idx_mgr_.open_index(name.c_str(), index);
+        indexes_[name] = handle;
+        return handle;
+    }
+    else {
+        return it->second;
+    }
 }
--- transaction/txn_manager.h
+++ transaction/txn_manager.h
@@ -1,15 +1,28 @@
 #pragma once
 
 #include "txn.h"
+#include "index_manager.h"
+#include "table_manager.h"
+
+#include <map>
+#include <string>
 
 class txn_manager
 {
 private:
-    
+    table_manager& tbl_mgr_;
+    index_manager& idx_mgr_;
+
+    std::map<std::string, table_handle> tables_;
+    std::map<std::string, index_handle> indexes_;
 public:
-    txn_manager();
+    txn_manager(index_manager& idx_mgr, table_manager& tbl_mgr);
     virtual ~txn_manager();
+    txn_manager(const txn_manager& mgr) = delete;
+    txn_manager& operator=(const txn_manager& mgr) = delete;
 
     virtual txn* start(txn_id_t id) = 0;
     
+    table_handle get_table(const std::string& name);
+    index_handle get_index(const std::string& name, int index);
 };
